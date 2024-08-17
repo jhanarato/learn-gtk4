33fc22b96a1f4df7b8971000f0946107c9a2c5e0
Ajahn Jhanarato <jhanarato@gmail.com>
2024-08-17 10:50:48 +0800

    New ToolbarView. Doesn't work yet.

---
+++ src/adwaita_toolbar_view.py
@@ -0,0 +1,21 @@
+import gi
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+from gi.repository import Adw, Gtk
+
+
+@Gtk.Template(filename="adwaita_toolbar_view.ui")
+class FormWindow(Adw.ApplicationWindow):
+    __gtype_name__ = "form-window"
+
+
+def on_activate(app):
+    win = FormWindow(application=app)
+    win.present()
+
+
+app = Adw.Application(application_id='org.bswa.jhanarato.AdwaitaToolbarViewWindow')
+app.connect('activate', on_activate)
+
+app.run(None)
