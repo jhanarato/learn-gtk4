03c47f72cf55fd3b451b26b716b91ec425112485
Ajahn Jhanarato <jhanarato@gmail.com>
2024-08-29 13:36:06 +0800

    Add clicked signal to nested button.

--- src/nested_templates.py
+++ src/nested_templates.py
@@ -9,6 +9,10 @@ from gi.repository import Adw, Gtk
 class NestedButton(Gtk.Button):
     __gtype_name__ = "NestedButton"
 
+    @Gtk.Template.Callback()
+    def nested_button_clicked(self, button):
+        print("Nested button clicked")
+
 
 @Gtk.Template(filename="templates/nesting_window.ui")
 class NestingWindow(Adw.ApplicationWindow):
